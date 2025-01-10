82ed85160b5e6d728c0b138b5525eacc70274a74
jhonny <jnony100@gmail.com>
2025-01-10 00:16:06 -0500

    cubatas lading page

--- src/app/Cubatas/page.tsx
+++ src/app/Cubatas/page.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import Head from "next/head"; // Importa el componente Head
 import LandingPage from "@/app/components/LadingPage";
 
 export default function CubatasPage() {
-    return(
+    return (
         <div>
+            {/* Modifica el título de la página */}
+            <Head>
+                <title>Mi Página de Cubatas</title>
+            </Head>
             <LandingPage />
         </div>
-    )
+    );
 }
